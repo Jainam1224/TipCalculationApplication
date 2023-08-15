29f019bde90d91cba2e2020d64ea4a9dcd567d6c
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2023-08-15 16:59:22 +0530

    immer using produce function in all acionstypes

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,41 +1,70 @@
+import produce from "immer";
 import {
   ITEM_ADDED,
   ITEM_PRICE_UPDATED,
   ITEM_REMOVED,
-  ITEM_QUANTITY_UPDATED
-} from './actions';
+  ITEM_QUANTITY_UPDATED,
+} from "./actions";
 
 let id = 1;
 
 export const initialItems = [
-  { uuid: id++, name: 'Awesome Tofu Roast', price: 14, quantity: 1 },
-  { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
+  { uuid: id++, name: "Awesome Tofu Roast", price: 14, quantity: 1 },
+  { uuid: id++, name: "Vegan Ham Sandwich", price: 12, quantity: 1 },
 ];
 
 export const reducer = (state = initialItems, action) => {
+  // if (action.type === ITEM_ADDED) {
+  //   const item = { uuid: id++, quantity: 1, ...action.payload };
+  //   return [...state, item];
+  // }
+
+  // Using Immer package which gives us the produce
   if (action.type === ITEM_ADDED) {
-    const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
+    produce(state, (draftState) => {
+      const item = { uuid: id++, quantity: 1, ...action.payload };
+      draftState.push(item);
+    });
   }
+
   if (action.type === ITEM_REMOVED) {
     return state.filter((item) => item.uuid !== action.payload.uuid);
   }
+
+  // if (action.type === ITEM_PRICE_UPDATED) {
+  //   return state.map((item) => {
+  //     if (item.uuid === action.payload.uuid) {
+  //       return { ...item, price: action.payload.price };
+  //     }
+  //     return item;
+  //   });
+  // }
+
+  // Using Immer package
   if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price };
-      }
-      return item;
+    return produce(state, (draftState) => {
+      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      item.price = parseInt(action.payload.price, 10);
     });
   }
+
+  // if (action.type === ITEM_QUANTITY_UPDATED) {
+  //   return state.map((item) => {
+  //     if (item.uuid === action.payload.uuid) {
+  //       return { ...item, quantity: action.payload.quantity };
+  //     }
+  //     return item;
+  //   });
+  // }
+
+  // Using immer package
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-      return item;
+    return produce(state, (draftState) => {
+      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      item.quantity = parseInt(action.payload.quantity, 10);
     });
   }
+
   return state;
 };
 
